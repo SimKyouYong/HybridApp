136781eefcc376d31d835dfbaf6d7b79ce54c269
SKY <SKY@DESKTOP-33A8MLH>
2017-03-02 16:28:54 +0900

    신규파일추가

---
+++ src/co/kr/hybridapp/SubNotActivity.java
@@ -0,0 +1,16 @@
+package co.kr.hybridapp;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SubNotActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+		
+		
+	}
+
+}
